c0c7617096777df25a32d7a7b5ff53d08d847d01
AkhilGurrapu <akhilgur123@gmail.com>
2023-06-19 14:17:21 -0500

    build

--- lib/changes/links.dart
+++ lib/changes/links.dart
@@ -1,6 +1,6 @@
 // CV
 const String resume =
-    'https://drive.google.com/file/d/1bDNNrkQtNVJbXcdwMSx0crQQvAF_nMwV/view?usp=sharing';
+    'https://drive.google.com/file/d/18W4iMhjQA1ltXAtwSmb9-nUvisbBrxiu/view?usp=sharing';
 
 // Github Link
 const String gitHub = 'https://github.com/akhilgurrapu';
